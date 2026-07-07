57cd4871247b1adae44fde7fb7d2b80fbc98645e
Zachary Breedlove <zkbreedlove@valdosta.edu>
2026-07-06 21:32:03 -0400

    Update Reservation model fields

--- src/main/java/reservationsystem/model/Reservation.java
+++ src/main/java/reservationsystem/model/Reservation.java
@@ -1,48 +1,49 @@
 package reservationsystem.model;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Reservation {
 	
-	 private final int id;
-	    private final int spaceId;
-	    private final String reserverName;
-	    private final String purpose;
-	    private final LocalDateTime startTime;
-	    private final LocalDateTime endTime;
+	private final int id;
+    private final int spaceId;
+    private final String userId;
+    private final LocalDate date;
+    private final LocalTime startTime;
+    private final LocalTime endTime;
 
-	    public Reservation(int id, int spaceId, String reserverName, String purpose,
-	                       LocalDateTime startTime, LocalDateTime endTime) {
-	        this.id = id;
-	        this.spaceId = spaceId;
-	        this.reserverName = reserverName;
-	        this.purpose = purpose;
-	        this.startTime = startTime;
-	        this.endTime = endTime;
-	    }
+    public Reservation(int id, int spaceId, String userId, LocalDate date,
+                       LocalTime startTime, LocalTime endTime) {
+        this.id = id;
+        this.spaceId = spaceId;
+        this.userId = userId;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 
-	    public int getId() {
-	        return id;
-	    }
+    public int getId() {
+        return id;
+    }
 
-	    public int getSpaceId() {
-	        return spaceId;
-	    }
+    public int getSpaceId() {
+        return spaceId;
+    }
 
-	    public String getReserverName() {
-	        return reserverName;
-	    }
+    public String getUserId() {
+        return userId;
+    }
 
-	    public String getPurpose() {
-	        return purpose;
-	    }
+    public LocalDate getDate() {
+        return date;
+    }
 
-	    public LocalDateTime getStartTime() {
-	        return startTime;
-	    }
+    public LocalTime getStartTime() {
+        return startTime;
+    }
 
-	    public LocalDateTime getEndTime() {
-	        return endTime;
-	    }
+    public LocalTime getEndTime() {
+        return endTime;
+    }
 
 }
