ca9bbb13957c6f2e8d68450cb805b88132a89267
Alex ORourke <aforourke888@gmail.com>
2026-07-08 21:44:13 -0500

    Build US-1 JavaFX Space List View

--- src/main/java/reservationsystem/Reservation.java
+++ src/main/java/reservationsystem/Reservation.java
@@ -1,7 +1,24 @@
 package reservationsystem;
 
-public class Reservation {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import reservationsystem.view.SpaceListView;
+
+public class Reservation extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        SpaceListView spaceListView = new SpaceListView();
+
+        Scene scene = new Scene(spaceListView.createView(), 700, 400);
+
+        stage.setTitle("Reservation System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Sprint project started");
+        launch(args);
     }
 }
